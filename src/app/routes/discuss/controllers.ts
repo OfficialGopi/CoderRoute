67761332082743684a7bafbaaebffc8efd5e1d88
OfficialGopi <official.gopi@outlook.com>
2025-07-07 15:40:02 +0530

    feat: Add discuss routes and controller implementation

---
+++ src/app/routes/discuss/controllers.ts
@@ -0,0 +1,7 @@
+import { STATUS_CODE } from "../../constants/statusCodes.constants";
+import { ApiResponse } from "../../utils/api-response";
+import { AsyncHandler } from "../../utils/async-handler";
+
+class DiscussController {}
+
+export { DiscussController };
--- src/app/versions/v1.ts
+++ src/app/versions/v1.ts
@@ -2,8 +2,8 @@ import express from "express";
 import { register as registerAuthRoutes } from "../routes/auth/routes";
 import { register as registerProblemRoutes } from "../routes/problem/routes";
 import { register as registerExecuteCodeRoutes } from "../routes/execute-code/routes";
-// import { register as registerPlaylistRoutes } from "../routes/playlist/routes";
-// import { register as registerSubmissionRoutes } from "../routes/submission/routes";
+import { register as registerPlaylistRoutes } from "../routes/playlist/routes";
+import { register as registerSubmissionRoutes } from "../routes/submission/routes";
 
 function v1(): express.Router {
   const router = express.Router();
@@ -12,8 +12,8 @@ function v1(): express.Router {
   router.use("/auth", registerAuthRoutes());
   router.use("/problem", registerProblemRoutes());
   router.use("/execute-code", registerExecuteCodeRoutes());
-  // router.use("/submission", registerSubmissionRoutes());
-  // router.use("/playlist", registerPlaylistRoutes());
+  router.use("/submission", registerSubmissionRoutes());
+  router.use("/playlist", registerPlaylistRoutes());
 
   return router;
 }
